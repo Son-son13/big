8b46147d2335d3058921a948fdd3583dcfe33639
Son-son13 <118826588+Son-son13@users.noreply.github.com>
2023-02-09 22:43:18 +0300

    Add files via upload

---
+++ map_utils.py
@@ -0,0 +1,104 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QLineEdit
+from PyQt5 import QtGui
+from PyQt5.QtCore import QSize
+
+from PyQt5.QtGui import QPixmap
+from PIL.ImageQt import ImageQt
+
+from api_utils import *
+from map_show import *
+
+class myMap:
+    def __init__(self):
+        self.z = 11
+        self.lat = 37.619585
+        self.lon = 55.865172
+
+
+my_map = myMap()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setMinimumSize(QSize(600, 600))  # Устанавливаем размеры
+        self.setWindowTitle("Большая задача")  # Устанавливаем заголовок окна
+        self.central_widget = QWidget(self)  # Создаём центральный виджет
+        self.setCentralWidget(self.central_widget)  # Устанавливаем центральный виджет
+        self.grid_layout = QGridLayout()  # Создаём QGridLayout
+        self.central_widget.setLayout(self.grid_layout)  # Устанавливаем данное размещение в центральный виджет
+        # метка и кнопки
+        self.label = QLabel()
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(50)
+        self.label.setFont(font)
+        self.label.setText("Адрес:")
+        self.grid_layout.addWidget(self.label, 0, 0, 1, 2)  # Добавляем метку в сетку
+        # карта
+        self.image = QLabel()
+        self.grid_layout.addWidget(self.image, 1, 3, 10, 10)
+        self.pixmap = QPixmap("empty.png")
+        self.image.setPixmap(self.pixmap)
+
+        # поле
+        self.adress = QLineEdit()
+        self.adress.setFont(font)
+        self.grid_layout.addWidget(self.adress, 0, 3, 1, 10)  # Добавляем поле в сетку
+        self.adress.setText('Пестеля, 8г')
+        # поле для вывода изображения карты
+        # кнопка Найти
+        self.btn2 = QPushButton("Найти", self)
+        self.grid_layout.addWidget(self.btn2, 0, 13, 1, 2)  # Добавляем кнопку в сетку
+        self.btn2.clicked.connect(self.new_search)
+
+    def new_search(self):
+        text = self.adress.text()
+        print(text)
+        print(geocode(text))
+        print(get_coords(text))
+        print(get_ll_spn(text))
+        print(my_map.lon, my_map.lat, my_map.z)
+        my_map.lon, my_map.lat = get_coords(text)
+        ll, spn = get_ll_spn(text)
+        params = {
+            "ll": ll,
+            "z": str(my_map.z),
+            'spn': spn,
+            "l": "map",
+            'pt': f"{my_map.lon},{my_map.lat}"
+        }
+
+        api_map = 'http://static-maps.yandex.ru/1.x/'
+        response = requests.get(api_map, params=params)
+
+        if not response:
+            print("Ошибка выполнения запроса")
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+        self.pixmap = QPixmap(map_file)
+        self.image.setPixmap(self.pixmap)
+
+
+app = QApplication(sys.argv)
+mw = MainWindow()
+mw.show()
+sys.exit(app.exec())
+
+#ll -
+#lon, lat -
+#z -
+#l -
+#spn -
+
+
+
+
---
+++ умуте.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtCore import QSize
+
+
+class ExampleWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(QSize(320, 240))
+        self.setWindowTitle('Example')
+
+        central_widget = QWidget(self)
+        grid_layout = QGridLayout(central_widget)
+        self.setCentralWidget(central_widget)
+
+        self._label = QLabel('', self)
+        self._label.setAlignment(QtCore.Qt.AlignCenter)
+        grid_layout.addWidget(self._label, 0, 0)
+
+        self._counter = 0
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_W:
+            self._counter += 1
+            self._label.setText('Клавиша Q нажата {} раз'.format(self._counter))
+        event.accept()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = ExampleWindow()
+    main_window.show()
+    sys.exit(app.exec_())
